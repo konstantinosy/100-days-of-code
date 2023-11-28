0f46d8c62128d9a885a3dc739fa12412f8be99d4
konstantinosy <kgiasemis@nyc.gr>
2023-11-28 21:32:37 +0200

    formatted the document with pep8

--- day1.py
+++ day1.py
@@ -1,16 +1,16 @@
-#1. Create a greeting for your program.
+# 1. Create a greeting for your program.
 
-#2. Ask the user for the city that they grew up in.
+# 2. Ask the user for the city that they grew up in.
 
-#3. Ask the user for the name of a pet.
+# 3. Ask the user for the name of a pet.
 
-#4. Combine the name of their city and pet and show them their band name.
+# 4. Combine the name of their city and pet and show them their band name.
 
-#5. Make sure the input cursor shows on a new line:
+# 5. Make sure the input cursor shows on a new line:
 
 print("Welcome to the Band Name Generator!")
 
-city = input("What city did you grow up in?\n") 
+city = input("What city did you grow up in?\n")
 
 petName = input("What is your pet's name?\n")
 
