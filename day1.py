e960280f0942bf3e419779606e01548bf4c61797
lexaproxy <147662922+konstantinosy@users.noreply.github.com>
2023-11-28 20:18:01 +0200

    Create day1.py
    Exercise 1. Really basic stuff such as I/O, variables and string concatenation.

---
+++ day1.py
@@ -0,0 +1,17 @@
+#1. Create a greeting for your program.
+
+#2. Ask the user for the city that they grew up in.
+
+#3. Ask the user for the name of a pet.
+
+#4. Combine the name of their city and pet and show them their band name.
+
+#5. Make sure the input cursor shows on a new line:
+
+print("Welcome to the Band Name Generator!")
+
+city = input("What city did you grow up in?\n") 
+
+petName = input("What is your pet's name?\n")
+
+print("A possible band name could be: " + city + " " + petName)
